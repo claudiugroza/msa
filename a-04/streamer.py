2c30ff981ef6265d82799ef69ee033d32b07a45e
Claudiu Groza <groza.claudiu9217@gmail.com>
2018-03-18 16:54:53 +0200

    Add the fourth assignment workspace

---
+++ a-04/streamer.py
@@ -0,0 +1,42 @@
+from twython import TwythonStreamer
+
+
+# generated credentials from Twitter API
+CONSUMER_KEY = 'my_consumer_key'
+CONSUMER_SECRET = 'my_consumer_secret'
+TOKEN_KEY = 'my_token_key'
+TOKEN_SECRET = 'my_token_secret'
+
+
+class NearbyTwythonStreamer(TwythonStreamer):
+
+    def __init__(self, nearby, api_key, api_secret, token_key, token_secret):
+        super(NearbyTwythonStreamer, self).__init__(app_key=api_key,
+                                                    app_secret=api_secret,
+                                                    oauth_token=token_key,
+                                                    oauth_token_secret=token_secret)
+        self.nearby = nearby
+
+    def on_success(self, data):
+        if 'text' in data:
+            print 'new post: {0}'.format(data['text'])
+
+    def on_error(self, status_code, data):
+        print '{0}:{1}'.format(status_code, data['text'])
+
+    def find_nearby(self):
+        self.statuses.filter(track=self.nearby)
+
+
+def listen_stream(keyword):
+    streamer = NearbyTwythonStreamer(keyword,
+                                     CONSUMER_KEY,
+                                     CONSUMER_SECRET,
+                                     TOKEN_KEY,
+                                     TOKEN_SECRET)
+    streamer.find_nearby()
+
+
+
+hashtag = '#iotupt'
+listen_stream(hashtag)
---
+++ a-04/tweet.py
@@ -0,0 +1,24 @@
+from twython import Twython
+import socket
+import time
+
+# generated credentials from Twitter API
+CONSUMER_KEY = 'my_consumer_key'
+CONSUMER_SECRET = 'my_consumer_secret'
+TOKEN_KEY = 'my_token_key'
+TOKEN_SECRET = 'my_token_secret'
+
+
+def post_tweet():
+    # form an unique tweet (cannot tweet the same text twice)
+    tweet_text = '#iotupt {0} started assignment at {1}'.format(socket.gethostname(), time.strftime('%H:%M:%S'))
+    # build twython object based on supplied credentials
+    twitter_api = Twython(CONSUMER_KEY, CONSUMER_SECRET, TOKEN_KEY, TOKEN_SECRET)
+    # update twitter status ~ post a tweet
+    twitter_api.update_status(status=tweet_text)
+    # log posted message for easy tracking
+    print 'posted: ', tweet_text
+
+
+# tweet once
+post_tweet()

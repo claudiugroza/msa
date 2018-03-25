3de58f9529597e153ec5ea2877b153929b7520df
Claudiu Groza <groza.claudiu9217@gmail.com>
2018-03-25 14:16:03 +0300

    Add fifth assignment instructions

---
+++ a-05/main.py
@@ -0,0 +1,55 @@
+import temperature #temperature stub
+import plotly.plotly as plot #plotly API
+import json #extract credentials
+import time #delay
+import datetime #temp-time chart association
+
+def init_plotly_stream():
+    # init credentials
+    with open('./credentials.json') as credentials_file:
+        plotly_user_config = json.load(credentials_file)
+
+    # authenticate with provided credentials
+    plot.sign_in(plotly_user_config["plotly_username"], plotly_user_config["plotly_api_key"])
+
+    # configure the plot
+    url = plot.plot([
+        {
+            'x': [],
+            'y': [],
+            'type': 'scatter',
+            'mode':'lines+markers',
+            'stream': {
+                'token': plotly_user_config['plotly_streaming_tokens'][0],
+                'maxpoints': 200
+                },
+        }], filename='MS-Temperature')
+
+    print "View your streaming graph here: ", url
+
+    # attach a stream to the plot
+    stream = plot.Stream(plotly_user_config['plotly_streaming_tokens'][0])
+
+    return stream
+
+def read_and_post(stream):
+    # read temperature from our stub module
+    temp = temperature.read_temp()
+    # post data to plotly
+    stream.write({'x': datetime.datetime.now(), 'y': temp})
+
+def main():
+    # stream setup
+    stream = init_plotly_stream()
+    stream.open()
+
+    while True:
+        # it's time to perform a reading
+        read_and_post(stream)
+        # delay between posts
+        time.sleep(2)
+
+try:
+    main()
+finally:
+    pass
